045a9bcdc6dbdf54a3c08c9d0e0846db31032b96
josepharcher <joseph.archer1@marist.edu>
2016-02-23 11:58:11 -0500

    Made even more lex improvements, might be done but not sure

--- dist/scripts/StateTable.js
+++
@@ -1,9 +0,0 @@
-var JOEC;
-(function (JOEC) {
-    var StateTable = (function () {
-        function StateTable() {
-        }
-        return StateTable;
-    })();
-    JOEC.StateTable = StateTable;
-})(JOEC || (JOEC = {}));
